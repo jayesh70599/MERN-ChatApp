f857dfbbbbd330efad555878e5d940add5cfd90c
jayesh70599 <jayesh70599@gmail.com>
2024-07-05 16:41:33 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import userRoutes from "./routes/user.routes.js";
 import authRoutes from "./routes/auth.routes.js";
@@ -11,6 +12,8 @@ import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+ 
 dotenv.config();
 
 app.use(express.json()); // to parse the incoming requests with json payloads
@@ -20,6 +23,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+})
 // app.get("/", (req, res) => {
 //     res.send("Hello World!");
 // });
